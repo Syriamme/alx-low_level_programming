131e08fccdc103d932dd34f654bef8d0d7370b1c
Syriamme <Roridaniel01@gmail.com>
2023-02-17 13:46:16 -0800

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+	int x;
+	int k;
+	int p;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			for (k = m; k <= '9'; k++)
+			{
+				for (p = (x + 1); p <= '9'; p++)
+				{
+					putchar(m);
+					putchar(x);
+					putchar(' ');
+					putchar(k);
+					putchar(p);
+					if (!((m == '9' && x == '8') && (k == '9' && p == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				p = '0';
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
