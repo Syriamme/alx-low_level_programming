0b0b4886a95c3bde068ecca5c5adfea1d1ce1bc0
Syriamme <Roridaniel01@gmail.com>
2023-02-17 13:32:09 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -10,22 +11,21 @@ int main(void)
 	int b;
 
 	for (a = '0'; a <= '9'; a++)
+
 	{
 		for (b = (a + 1); b <= '9'; b++)
 		{
 			putchar(a);
 			putchar(b);
 
-			if (a > '9' || b > '9')
-			{
-				break;
-			}
+			if ((a != '8') || (b != '9'))
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
-	};
+	}
+	putchar('\n');
 
 	return (0);
 }
