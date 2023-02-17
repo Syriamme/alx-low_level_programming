305cd77aa4c4d53e5e09bd793964b554b64ee627
Syriamme <Roridaniel01@gmail.com>
2023-02-17 10:47:20 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int abc;
+
+	for (abc = 0; abc < 9; abc++)
+
+		putchar(abc);
+
+	return (0);
+}
