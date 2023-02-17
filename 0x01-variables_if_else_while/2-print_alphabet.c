1c2e7a07fe676164b0a7fa3c084c061751b64f82
Syriamme <Roridaniel01@gmail.com>
2023-02-17 08:37:38 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		putchar(abc);
+	}
+		putchar('\n');
+	return (0);
+}
