76fdb914a1a7e4b61937d200c445ce159782f23c
Syriamme <Roridaniel01@gmail.com>
2023-02-17 10:04:45 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		if (abc == 'e' || abc == 'q')
+		break;
+	}
+	{
+		putchar(abc);
+	}
+		return (0);
+}
