9ae50d57f1bfd772dfe7733091150d4a2b99c03d
Syriamme <Roridaniel01@gmail.com>
2023-02-21 09:10:02 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
