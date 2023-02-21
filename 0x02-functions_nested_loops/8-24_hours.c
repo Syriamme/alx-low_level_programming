05cd425295c79d71ac7e6d7a4bb2327d13390d9f
Syriamme <Roridaniel01@gmail.com>
2023-02-21 14:01:50 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day
- * Description: Prints from 00:00 to 23:59
+ * jack_bauer - Printing every minute of the day
+ * Description: Printing from 00:00 to 23:59
  */
 
 void jack_bauer(void)
