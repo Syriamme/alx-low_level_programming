caae44bc69ff86f5ce742845bfd92ac1618571c1
Syriamme <Roridaniel01@gmail.com>
2023-02-27 07:33:01 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters
+ *@str: The string
+ *
+ *Return: Void
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	while (str[len] != '\0')
+	{
+		if (len % 2 == 0)
+			_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
