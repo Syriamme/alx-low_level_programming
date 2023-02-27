157a7bc62eecfc64ebfda206b1e674f78a6d7b6f
Syriamme <Roridaniel01@gmail.com>
2023-02-27 04:21:00 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,18 +9,16 @@
 
 void print_rev(char *s)
 {
-    int length = 0;
-    while (s[length] != '\0')
-    {
-        length++
-    }
-
-    for (int i= length - 1; 1>= 0; i--)
-    {
-        _putchar (s[i]);
-    }
-    
-    {
-        _putchar ('\n');
-    }
+	int length = 0;
+	while (s[length] != '\0')
+	{
+		length++
+	}
+	for (int i= length - 1; 1>= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	{
+		_putchar ('\n');
+	}
 }
