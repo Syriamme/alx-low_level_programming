3b9a53b37bca62b92bba827db6925f6630c90abf
Syriamme <roridaniel01@gmail.com>
2023-02-27 14:15:50 +0300

    Updated Kevsam solutions

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear Kevsam. I fear who Kevsam will be - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
