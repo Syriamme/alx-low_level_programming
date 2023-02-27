91106eec404e3544549a5bea46bddb18b71a1ebb
Syriamme <Roridaniel01@gmail.com>
2023-02-27 02:26:21 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,7 @@
 
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++
-	}
-	_putchar('\n');
+	while (*str != '\0')
+		_putchar(*str++);
+		_putchar('\n');
 }
