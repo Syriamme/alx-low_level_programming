b58713d93b1d60651f4c681e56441ecf6925fc8d
Syriamme <Roridaniel01@gmail.com>
2023-03-02 12:03:59 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,40 +8,34 @@
 
 char *cap_string(char *str)
 {
-	int i, k;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
+
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			str[i] -= 'a' - 'A';
+			i++;
 		}
-		if (str[i] == ',' || str[i] == ';' ||
-				str[i] == '.' ||
-				str[i] == '!' ||
-				str[i] == '?' ||
-				str[i] == '"' ||
-				str[i] == '(' ||
-				str[i] == ')' ||
-				str[i] == '{' ||
-				str[i] == ' ' ||
-				str[i] == '}' ||
-				str[i] == '\t' ||
-				str[i] == '\n')
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
 		{
-			for (k = i + 1; str[k] != '\0'; k++)
-			{
-				if (str[k] >= 'a' && str[k] <= 'z')
-				{
-					str[k] -= 'a' - 'A';
-				}
-				else
-				{
-					break;
-				}
-			}
-			i = k - 1;
+			str[i] -= 32;
 		}
+		i++;
 	}
 	return (str);
 }
