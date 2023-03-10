7c5158fcd583159e8b63bb74057156a070684ab5
Syriamme <Roridaniel01@gmail.com>
2023-03-10 12:00:35 -0800

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,11 @@
  *@argv: argument vector
  *Return: Always 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf ("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
