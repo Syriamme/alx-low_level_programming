a8e02ee39881ba12fe7469f86b885c25f867252e
Syriamme <Roridaniel01@gmail.com>
2023-03-10 11:59:01 -0800

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry Point
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always 0
+ */
+int main (int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf ("%s\n", argv[0]);
+	}
+	return (0);
+}
