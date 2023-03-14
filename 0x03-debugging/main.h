5452f61a6589cec4aa5a02fea2e5b0a0ed496541
Syriamme <Roridaniel01@gmail.com>
2023-03-14 10:46:42 -0700

    hot fixes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,8 +6,6 @@
 
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
-
 int main(void);
 
 int largest_number(int a, int b, int c);
@@ -15,3 +13,5 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
+
+#endif /* MAIN_H */
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +12,5 @@ char *cap_string(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif /* MAIN_H */
