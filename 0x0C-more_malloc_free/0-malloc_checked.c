ece6109bfc95afd6a63d50021e248d5ba538a810
Syriamme <Roridaniel01@gmail.com>
2023-03-16 09:57:44 -0700

    memory allocation

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- *malloc_checked: a function that allocates memory
+ *malloc_checked- a function that allocates memory
  *@b: integer
  *Return: a pointet to allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	char *ptr;
-	
+
 	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
