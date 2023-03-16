7d6b0f9b938598b11c4aebf8d833c154ee5c15b2
Syriamme <Roridaniel01@gmail.com>
2023-03-16 10:48:46 -0700

    string concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -10,33 +11,25 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int len1 = 0, len2 = 0, i, j;
-        char *ptr;
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *ptr;
 
-        if (s1 == NULL)
-                s1 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0' && len2 < n)
+		len2++;
 
-        if (s2 == NULL)
-                s2 = "";
-
-        while (s1[len1] != '\0')
-                len1++;
-
-        while (s2[len2] != '\0' && len2 < n)
-                len2++;
-
-        ptr = malloc(sizeof(*ptr) * (len1 + len2 + 1));
-
-        if (ptr == NULL)
-                return (NULL);
-
-        for (i = 0; i < len1; i++)
-                ptr[i] = s1[i];
-
-        for (j = 0; j < len2; j++)
-                ptr[i + j] = s2[j];
-
-        ptr[i + j] = '\0';
-
-        return (ptr);
+	ptr = malloc(sizeof(*ptr) * (len1 + len2 + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < len2; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return (ptr);
 }
