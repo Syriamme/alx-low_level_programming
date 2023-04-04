75e8d0345cecc05f3fc829c33582bc39e380ef45
Syriamme <roridaniel01@gmail.com>
2023-04-04 19:28:17 +0300

    inserting a node at a position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		newnode->next = *head;
 		*head = newnode;
+		return (newnode);
 	}
 	tp = *head;
 	for (sum = 0; sum < idx; sum++)
