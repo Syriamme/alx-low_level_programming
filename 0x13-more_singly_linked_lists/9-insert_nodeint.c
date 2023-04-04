6650814f17c47c4de843ea10f06277148bd32b73
Syriamme <roridaniel01@gmail.com>
2023-04-04 19:17:57 +0300

    inserting a node at a position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	newnode->n = n;
-	newmode->next = NULL;
+	newnode->next = NULL;
 	
 	if (idx == 0)
 	{
