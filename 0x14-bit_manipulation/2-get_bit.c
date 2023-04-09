cdb9784df13856106190314239b2520c188712ea
Syriamme <roridaniel01@gmail.com>
2023-04-09 03:36:52 -0700

    a function to get the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,17 +10,15 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int siz_t;
 	unsigned long int mask, value;
-	
+
 	siz_t = (8 * sizeof(unsigned long int));
-	
 
 	if (index >= siz_t * 8)
 		return (-1);
-	else
+	else if
 	{
 		mask = n >> index;
 		value = ((mask) & 1);
-		
 		return (value);
 	}
 }
