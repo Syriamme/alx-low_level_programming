f01b0ab4a7eabc4fa6ab87e6630d0b315cf43e47
Syriamme <roridaniel01@gmail.com>
2023-04-09 11:03:51 -0700

    function that returns the number of bits you would need

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int temp;
 	unsigned int result = 0;
 
-	temp = n ^ m;
+	temp = (n ^ m);
 	while (temp != '0')
 	{
 		result += (temp & 1);
