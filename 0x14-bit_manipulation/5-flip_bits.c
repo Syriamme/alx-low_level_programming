22515b5389f17c60611c31332e2bbabe1b079de0
Syriamme <roridaniel01@gmail.com>
2023-04-09 21:10:08 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-flip_bits.c -o f

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,8 +11,8 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int temp;
 	unsigned int result = 0;
 
-	temp = (n ^ m);
-	while (temp != '0')
+	temp = n ^ m;
+	while (temp != 0)
 	{
 		result += (temp & 1);
 		temp >>= 1;
