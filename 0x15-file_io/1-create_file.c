88b726d01994c9e7e525ac415de6c57326b248a9
Syriamme <roridaniel01@gmail.com>
2023-04-11 11:50:35 +0300

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,37 +8,32 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int len = 0, check = 0;
-	FILE *ptr;
-
+	int fp, len = 0, check = 0;
+	
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	ptr = fopen(filename, "w");
-	if (!ptr)
+	fp = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fp == -1)
 	{
 		return (-1);
 	}
+	
 	if (!text_content)
 	{
 		while (text_content[len] != '\0')
 		{
 			len++;
 		}
-		check = fwrite(text_content, sizeof(char), len, ptr);
-		if (check != len)
+		check = write(fp, text_content, len);
+		if (check != len || check == 0)
 		{
-			fclose(ptr);
-			return (-1);
-		}
-		if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
-		{
-			fclose(ptr);
+			close(fp);
 			return (-1);
 		}
 	}
-	fclose(ptr);
+	close(fp);
 	return (1);
 }			
